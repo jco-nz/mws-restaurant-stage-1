c33e66c1b789d6ced8b245faf7118f8c11c2b88a
Joseph Co <jcohai@yahoo.com>
2019-03-18 22:02:34 +1300

    Add Mapbox  token

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiamNvaGFpIiwiYSI6ImNqdGNnbjg1dzB3amM0Ym9hNnNhZTBzeHoifQ.z4ebZd7daiKWT54WdCCtPA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
